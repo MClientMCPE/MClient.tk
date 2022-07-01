04cbf8e35b1cc4986c5df8b67bf2d82fc66b15f3
hydroflame521 <87723789+hydroflame521@users.noreply.github.com>
2022-07-01 12:49:53 -0500

    Delete hydroflame521.js

--- hydroflame521.js
+++
@@ -1,7 +0,0 @@
-(function () {
-        var words = ["A Minecraft Pocket Edition Hack Client for Android","Powerful server bypasses","Expertly designed GUI and icons","All premium features are free","Active Discord Support and Development"],
-        i = 0;
-        setInterval(function(){ $('#words').fadeOut(function(){
-            $(this).html(words[(i = (i + 1) % words.length)]).fadeIn();
-          }); }, 6000)
-      })();
